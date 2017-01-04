a2cd7c4b5a01b57409cef49e3cad5f8f13af0f17
David Brear <davidbrear@Davids-MacBook-Pro.local>
2017-01-04 06:48:01 -0500

    initial commit

---
+++ spec/auto-import-js-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutoImportJsView from '../lib/auto-import-js-view';
+
+describe('AutoImportJsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
